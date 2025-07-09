724c2793753a5f4787efc5d337526517cebf1eb8
vkosheleva <v.kosheleva@optisoft.net>
2025-07-09 16:19:06 +0300

    + test remove task

---
+++ tests/remove-task.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  let taskToDelete = 'delectus aut autem';
+  await page.goto(`/`);
+  await page.waitForLoadState('networkidle');
+  await expect(page.getByText(taskToDelete)).toBeVisible();
+  await page
+    .getByText('delectus aut autem Edit Delete')
+    .locator('.delete-button')
+    .click();
+  await expect(page.getByText(taskToDelete)).toBeVisible({
+    visible: false,
+  });
+});
