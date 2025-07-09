3b6c1af9589cdb87069c430d655568e1281b4cca
vkosheleva <v.kosheleva@optisoft.net>
2025-07-09 15:40:32 +0300

    + test add_task

---
+++ tests/add-task.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('Add task', async ({ page }) => {
+  let taskName = `new task ${Date.now()}`;
+  await page.goto(`/`);
+  await page.waitForLoadState('networkidle');
+  await expect(page.getByRole('heading', { name: 'Todo List' })).toBeVisible();
+  await page.locator('.task-input').fill(taskName);
+  await expect(page.locator('.task-input')).toHaveValue(taskName);
+  await page.locator('.add-button').click();
+  await expect(page.getByText(taskName)).toBeVisible();
+  await page.close();
+});
